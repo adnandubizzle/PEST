b0523c72f5cb6703b9d55e1c0954837379807660
Adnan <adnan.umar@dubizzle.com>
2025-07-01 10:38:07 +0500

    Setup Project

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,12 @@
+<?php
+
+// namespace Tests\Unit;      //We dont need it in PEST
+
+// use PHPUnit\Framework\TestCase;  //we dont need it in PEST
+
+
+
+// We can use 'test' or 'it'. Both same.
+test('example', function () {
+    $this->assertTrue(true);
+});
